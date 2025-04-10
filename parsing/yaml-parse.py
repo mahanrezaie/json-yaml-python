4395f80959dc5e975a844a2aad610d57997660cc
mahan <rezaiemahan@gmail.com>
2025-04-10 20:43:10 +0330

    [ Feature ] Add open_file function

--- parsing/yaml-parse.py
+++ parsing/yaml-parse.py
@@ -1,1 +1,16 @@
+from yaml import safe_load, YAMLError
 
+def open_file(file):
+    try:
+        with open(file, "r") as f:
+            yaml_string = safe_load(f)
+            return yaml_string
+
+    except YAMLError as e:
+        print(f"Error: {file} is not a valid YAML - {e}")
+
+
+
+
+test = open_file("test.yaml")
+print(test)
