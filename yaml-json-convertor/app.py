1f5fab7a72f2725585b58cecea99976a621a34a5
mahan <rezaiemahan@gmail.com>
2025-04-12 16:55:20 +0330

    [Add] added app.py

---
+++ yaml-json-convertor/app.py
@@ -0,0 +1,24 @@
+import yaml
+import json
+def check_file_type(file: str):
+    with open(file, "r") as f:
+        content = f.read()
+
+    try:
+        json.loads(content)
+        return "JSON"
+    except json.JSONDecodeError:
+        pass
+
+    try:
+        if content.strip().startswith("---"): # it is common to use --- to start a yaml file
+            yaml.safe_load(content)
+            return "YAML"
+    except yaml.YAMLError:
+        pass
+
+    print(f"The file {file} is not JSON or YAML.")
+
+
+
+
