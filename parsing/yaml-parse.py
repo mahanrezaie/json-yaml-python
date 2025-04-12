8b0d11f3a12fdcb020aeb2479c5c03c9b452b9c3
mahan <rezaiemahan@gmail.com>
2025-04-12 16:08:58 +0330

    [ Feature ] Add key-value-devider function

--- parsing/yaml-parse.py
+++ parsing/yaml-parse.py
@@ -3,14 +3,19 @@ from yaml import safe_load, YAMLError
 def open_file(file):
     try:
         with open(file, "r") as f:
-            yaml_string = safe_load(f)
-            return yaml_string
+            yaml_dict = safe_load(f)
+            return yaml_dict
 
     except YAMLError as e:
         print(f"Error: {file} is not a valid YAML - {e}")
 
+def key_value_devider(yaml_dict: dict):
+    for key, value in yaml_dict.items():
+        print(f"{key}: {value}")
 
 
 
-test = open_file("test.yaml")
-print(test)
+test = open_file("parse.yaml")
+yaml_dict = key_value_devider(test)
+
+
